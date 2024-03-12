2a2b9d8d41b745278528a4958f3578182abde1a7
davifariasp <davi.informatica8@gmail.com>
2024-03-12 20:11:22 -0300

    adicionando conexao com database via excel e excluindo pasta output

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
 from docx import Document
 from datetime import datetime
+import pandas as pd
 
+tabela = pd.read_excel("./source/database.xlsx")
 
-try:
-    documento = Document("./source/contrato-original.docx")
+for linha in tabela.index:
+    try:
+        documento = Document("./source/contrato-original.docx")
 
-    nome = "Fulano de Tal"
-    item1 = "Carro"
-    item2 = "Celular"
-    item3 = "Notebook"
+        nome = tabela.loc[linha, "nome"]
+        item1 = tabela.loc[linha, "item1"]
+        item2 = tabela.loc[linha, "item2"]
+        item3 = tabela.loc[linha, "item3"]
+        dia = str(datetime.now().day)
+        mes = str(datetime.now().month)
+        ano = str(datetime.now().year)
 
-    dia = str(datetime.now().day)
-    mes = str(datetime.now().month)
-    ano = str(datetime.now().year)
+        referencias = {
+            "XXXX": nome,
+            "YYYY": item1,
+            "ZZZZ": item2,
+            "WWWW": item3,
+            "DD": dia,
+            "MM": mes,
+            "AAAA": ano
+        }
 
-    referencias = {
-        "XXXX": nome,
-        "YYYY": item1,
-        "ZZZZ": item2,
-        "WWWW": item3,
-        "DD": dia,
-        "MM": mes,
-        "AAAA": ano
-    }
+        for paragrafo in documento.paragraphs:
+            # print(paragrafo.text)
+            for codigo in referencias:
+                valor = referencias[codigo]
+                paragrafo.text = paragrafo.text.replace(codigo, valor)
 
-    for paragrafo in documento.paragraphs:
-        # print(paragrafo.text)
-        for codigo in referencias:
-            valor = referencias[codigo]
-            paragrafo.text = paragrafo.text.replace(codigo, valor)
+        nome_documento = f"Contrato - {nome}"
+        documento.save(f"./output/{nome_documento}.docx")
 
-    documento.save("./output/contrato-final.docx")
-except Exception as e:
-    print(e)
+    except Exception as e:
+        print(e)
 
